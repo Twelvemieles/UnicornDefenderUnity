7fe72d4d69692a747c1c918386b13d8230290f1f
Twelvemieles <juandimieles99@gmail.com>
2018-01-26 01:02:59 -0500

    versión 2

---
+++ Unicorn Defender UV/Assets/scripts/mananger/mananger.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class mananger : MonoBehaviour {
+    public int A;
+    public int B;
+    public int damage;
+    public int RangeLife;
+    public Text restartText;
+    private bool gameRestart;
+    public Text gameOverText;
+    private bool gameOver;
+    public int score;
+    public Text ScoreText;
+   
+    
+
+    // Use this for initialization
+    void Start() {
+        gameOver = false;
+        gameOverText.gameObject.SetActive(false);
+        gameRestart = false;
+        restartText.gameObject.SetActive(false);
+        score = 0;
+        UpdateScore();
+        RangeLife = 5;
+        AsteroidRange();
+    }
+
+    public void AddScore(int value)
+    {
+        score += value;
+        UpdateScore();
+    }
+    // Update is called once per frame
+    void Update() {
+
+    }
+    //cambiar el rango de la vida de los asteroides
+    IEnumerator AsteroidRange()
+    {
+
+        while (true)
+        {
+            yield return new WaitForSeconds(6);
+            A += RangeLife;
+            B += RangeLife;
+
+        }
+    }
+
+    void UpdateScore()
+    {
+        ScoreText.text = "" + score;
+    }
+
+    public void GameOver()
+    {
+    gameOverText.gameObject.SetActive(true);
+        gameOver = true;
+    }
+}
